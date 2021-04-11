72232b5ff2c49c9954af27cfaf393bf66818dc23
Loonride <22580625+Loonride@users.noreply.github.com>
2021-04-11 17:32:55 -0500

    add comments to new files

--- scripts/person_follower.py
+++ scripts/person_follower.py
@@ -16,18 +16,25 @@ class PersonFollower(object):
         rospy.Subscriber('/scan', LaserScan, self.process_scan)
 
     def process_scan(self, data):
+        """ This is a callback for the scan subscription. It processes the scan ranges
+        and determines the new robot speed and angular speed.
+        """
         range_index = 0
         range_dist = data.ranges[0]
+        # here we determine the distance and direction of the closest scanned object
         for i in range(len(data.ranges)):
             r = data.ranges[i]
             if r < range_dist:
                 range_index = i
                 range_dist = r
 
+        # if there are no objects within range, everything will be infinity
         if range_dist == math.inf:
             self.set_v(0, 0)
             return
 
+        # scale speed with distance until a nearby distance where the
+        # robot stops
         diff = range_dist - 0.5
         speed = 0
         if diff > 0:
@@ -36,12 +43,17 @@ class PersonFollower(object):
         range_index_dist = range_index
         angular_speed = 0.015 * range_index
         angular_speed_sign = 1
+        # decide whether the robot should turn left or right based on the
+        # position of the nearest object in the ranges array
         if range_index >= 180:
             range_index_dist = 360 - range_index
             angular_speed = 0.015 * (360 - range_index)
             angular_speed_sign = -1
 
         angular_speed = angular_speed_sign * angular_speed
+        # if the robot is too far and angled too much away from the target, the robot
+        # must turn a bit without any linear speed before it starts driving forward
+        # (not doing this can result in the robot mistakenly losing the target)
         if range_index_dist > 60 and range_dist > 2:
             speed = 0
 
--- scripts/wall_follower.py
+++ scripts/wall_follower.py
@@ -18,30 +18,44 @@ class WallFollower(object):
         self.angular_speed_sign = None
 
     def process_scan(self, data):
+        """ This is a callback for the scan subscription. It processes the scan ranges
+        and determines the new robot speed and angular speed.
+        """
         range_index = 0
         range_dist = data.ranges[0]
+        # here we determine the distance and direction of the closest scanned object
         for i in range(len(data.ranges)):
             r = data.ranges[i]
             if r < range_dist:
                 range_index = i
                 range_dist = r
 
+        # if there are no nearby objects, drive until we find a wall
         if range_dist == math.inf or range_dist > 1:
             self.set_v(0.4, 0)
             return
         
+        # here we work out which direction to turn and at what rate
+        # range_index_dist will end up being a number in the range 0-180
         range_index_dist = range_index
         angular_speed_sign = -1
         if range_index >= 180:
             angular_speed_sign = 1
             range_index_dist = 360 - range_index
 
+        # a direction is picked here that has more influence than the other, since
+        # we will be turning in the same direction around the square
         if self.angular_speed_sign is None:
             self.angular_speed_sign = angular_speed_sign
         speed = range_dist * 0.8
+        # when driving along a wall, we are trying to maintain a 90 degree angle with
+        # the wall, so generally turn rate scales with how far off we are from this
+        # angle
         turn_rate = abs(90 - range_index_dist)
 
         final_angular_speed_sign = self.angular_speed_sign
+        # we flip the final angular speed sign here in case the robot needs to correct
+        # a bit and move back towards the wall
         if range_index_dist - 90 > 0 and turn_rate < 20:
             final_angular_speed_sign = -1 * self.angular_speed_sign
             turn_rate *= 0.5
@@ -50,6 +64,8 @@ class WallFollower(object):
 
         angular_speed = 0.02 * turn_rate
 
+        # if the wall approaches the front of the robot and it comes too close, do
+        # some turning without driving forward to avoid driving into the wall
         if data.ranges[0] < 0.8:
             speed = 0
             angular_speed = 0.5
